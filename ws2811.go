187e40f101a2988a2fb87e7b61f33256051f2854
Adrian <adrianhonline+githib@gmail.com>
2019-04-23 16:43:32 +0200

    Update ws2811.go

--- ws2811.go
+++ ws2811.go
@@ -1,3 +1,24 @@
+    
+// Copyright 2018 Jacques Supcik / HEIA-FR
+// Copyright 2019 Adrian Houghton
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Interface to ws2811 chip (neopixel driver). Make sure that you have
+// ws2811.h and pwm.h in a GCC include path (e.g. /usr/local/include) and
+// libws2811.a in a GCC library path (e.g. /usr/local/lib).
+// See https://github.com/jgarff/rpi_ws281x for instructions
+
 package ws2811
 
 import "github.com/pkg/errors"
