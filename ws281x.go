9ad64de1caafd05ee47a050711393426f8a8515a
Adrian <adrianhonline+githib@gmail.com>
2019-04-23 16:43:56 +0200

    Update ws281x.go

--- ws281x.go
+++ ws281x.go
@@ -1,4 +1,5 @@
 // Copyright 2018 Jacques Supcik / HEIA-FR
+// Copyright 2019 Adrian Houghton
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
